e7f6a31bf83a06d12666a1aa682c8f3ef7b63ad4
manuel_j555 <programador.manuel@gmail.com>
2014-07-14 14:58:03 -0430

    Añadidos Tipos de Formularios y Subscribers
    La idea es facilitar el uso de las entidades en los formularios usando
    los tipo de campo "ciudad" y "estado":
    
    Ejemplo:
    
    $builder->add('city', 'ciudad');
    
    El Subscriber nos permite manejar la funcionalidad de actualizar las
    opciones del campo de la ciudad en base al valor del estado tanto en el
    modelo como en el formulario

---
+++ Form/Listener/AddCiudadSubscriber.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Manuel Aguirre Project.
+ *
+ * (c) Manuel Aguirre <programador.manuel@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SfVen\Bundle\RegionesBundle\Form\Listener;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\Exception\OutOfBoundsException;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Description of AddCiudadListener
+ *
+ * @author Manuel Aguirre <programador.manuel@gmail.com>
+ */
+class AddCiudadSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     *
+     * @var array
+     */
+    protected $options;
+
+    public function __construct(array $options = array())
+    {
+        $this->init($options);
+    }
+
+    protected function init(array $options)
+    {
+        $resolver = new OptionsResolver();
+
+        $this->setDefaultOptions($resolver);
+
+        $this->options = $resolver->resolve($options);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::POST_SET_DATA => 'addForm',
+            FormEvents::SUBMIT => 'addForm',
+        );
+    }
+
+    public function addForm(FormEvent $event, $name)
+    {
+        $form = $event->getForm();
+        $data = $event->getData();
+
+        if (!isset($form[$this->options['estado_name']])) {
+            throw new OutOfBoundsException(sprintf('Child "(estado_name => %s)" passed to AddCiudadSubscriber does not exist.', $this->options['estado_name']));
+        }
+
+        $estado = $form[$this->options['estado_name']]->getData();
+
+        $form->add($this->options['name'], $this->options['type'], array(
+            'query_builder' => function(EntityRepository $er) use ($estado) {
+        return $er->createQueryBuilder('ciudad')
+                        ->where('ciudad.estado = :val')
+                        ->setParameter('val', $estado);
+    }
+                ) + $this->options['options']);
+    }
+
+    public function setDefaultOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'name' => 'ciudad',
+            'type' => 'ciudad',
+            'options' => array(),
+            'estado_name' => 'estado',
+        ));
+    }
+
+}
